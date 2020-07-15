a48e70de977780dd533f475f6a280a8a7f866ffe
Rustum Jordan <rustumjordan@gmail.com>
2020-07-16 00:05:38 +0800

    Implementing Models and adding model to the admin page

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Rustum Jordan',
-        'title': 'Blog post 1',
-        'content': 'This is a post',
-        'date_posted': 'August 20, 2020'
-    },
-    {
-        'author': 'Jimmy Doe',
-        'title': 'Blog post 2',
-        'content': 'Hey guys',
-        'date_posted': 'August 12, 2020'
-    }
-]
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
     # return HttpResponse('<h1>Blog Home</h1>')
@@ -27,5 +13,3 @@ def home(request):
 def about(request):
     return render(request, 'blog/about.html', { 'title': 'About' })
     # return HttpResponse('<h1>About Us</h1>')
-BDO = 51,000
-bpi = 52,557
